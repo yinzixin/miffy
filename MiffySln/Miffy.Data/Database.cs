e659ff54c15bd0c9ec9f3ca89a832c26270cec18
yinzixin <yinzixin1985@hotmail.com>
2017-07-12 10:14:13 +0800

    add data

---
+++ MiffySln/Miffy.Data/Database.cs
@@ -0,0 +1,23 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miffy.Data
+{
+    public class Database
+    {
+        public static DbConnection GetConn()
+        {
+            //var str = "Server=172.200.2.3;Database=forest;User=root;Password=123;Charset=utf8;";
+            var str = System.Configuration.ConfigurationManager.ConnectionStrings["mysql"].ConnectionString;
+            var conn = new MySqlConnection(str);
+            conn.Open();
+            return conn;
+        }
+
+    }
+}
